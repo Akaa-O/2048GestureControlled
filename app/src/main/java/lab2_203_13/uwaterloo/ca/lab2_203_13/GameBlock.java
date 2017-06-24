f7b7f4f8540e9a265a8d09a8dd00f47e4aae394d
fliu98 <tianlunliu98@gmail.com>
2017-06-24 15:50:59 -0400

    GameBlock and GameLoopTask

--- app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameBlock.java
+++ app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameBlock.java
@@ -1,8 +1,54 @@
 package lab2_203_13.uwaterloo.ca.lab2_203_13;
 
+import android.content.Context;
+import android.widget.ImageView;
+
 /**
  * Created by desmond on 6/24/17.
  */
 
-public class GameBlock {
+public class GameBlock extends android.support.v7.widget.AppCompatImageView{
+
+    public enum Direction{
+        LEFT, RIGHT, UP, DOWN, STOPPED
+    }
+
+    private float x;
+    private float y;
+    private static final float IMAGE_SCALE = 1;
+    private Direction myDirection;
+
+    public GameBlock(Context myContext,float coordX, float coordY){
+        super(myContext);
+        myDirection = Direction.STOPPED;
+        x = coordX;
+        y = coordY;
+        this.setImageResource(R.drawable.gameblock);
+        this.setScaleX(IMAGE_SCALE);
+        this.setScaleY(IMAGE_SCALE);
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+
+    public void setX(float x){
+        this.x = x;
+    }
+
+    public void setY(float y){
+        this.y = y;
+    }
+
+    public Direction getDirection(){
+        return myDirection;
+    }
+
+    public void setDirection(Direction dir){
+        myDirection = dir;
+    }
 }
--- app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameLoopTask.java
+++ app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameLoopTask.java
@@ -1,8 +1,84 @@
 package lab2_203_13.uwaterloo.ca.lab2_203_13;
 
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.TimerTask;
+
 /**
  * Created by desmond on 6/24/17.
  */
 
-public class GameLoopTask {
+public class GameLoopTask extends TimerTask{
+
+    private Activity myActivity;
+    private ArrayList<GameBlock> myBlocks;
+    private static final float GRID_WIDTH = 30;
+    private static final float GRID_HEIGHT = 30;
+    private static final float X_MAX = 200;
+    private static final float Y_MAX = 200;
+
+    public GameLoopTask(Activity activity){
+        myActivity = activity;
+    }
+
+    @Override
+    public void run() {
+
+        //Set block new position based on direction
+
+        for (final GameBlock block : myBlocks) {
+
+            switch (block.getDirection()) {
+
+                case LEFT:
+                    if (block.getX() > 0) {
+                        block.setX(block.getX() - GRID_WIDTH);
+                    }
+                    break;
+
+                case RIGHT:
+                    if (block.getX() < X_MAX) {
+                        block.setX(block.getX() + GRID_WIDTH);
+                    }
+                    break;
+
+                case UP:
+                    if (block.getY() > 0) {
+                        block.setY(block.getY() - GRID_HEIGHT);
+                    }
+                    break;
+
+                case DOWN:
+                    if (block.getY() < Y_MAX) {
+                        block.setY(block.getY() + GRID_HEIGHT);
+                    }
+                    break;
+
+                default:
+            }
+
+
+            //Stop block
+
+            block.setDirection(GameBlock.Direction.STOPPED);
+
+            //Update Screen
+
+            myActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    block.setX(block.getX());
+                    block.setY(block.getY());
+                }
+            });
+
+        }
+    }
+
+    public void createBlock(){
+        GameBlock block = new GameBlock(myActivity, 10, 10);
+        myBlocks.add(block);
+    }
+
 }
