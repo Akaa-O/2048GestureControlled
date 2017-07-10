330eb47df2c87f4a2d0ed912dc421a8b9b0861f7
Akaa-O <akachukwu131@gmail.com>
2017-07-10 14:59:33 -0400

    number size adjustment

--- app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameBlock.java
+++ app/src/main/java/lab2_203_13/uwaterloo/ca/lab2_203_13/GameBlock.java
@@ -14,7 +14,8 @@ import java.util.Random;
 public class GameBlock extends GameBlockTemplate{
 
     private static final float IMAGE_SCALE = 0.66F;
-    private static final float TEXT_OFFSET = 175f;
+    private static final float yTEXT_OFFSET = 120f;
+    private static final float xTEXT_OFFSET = 105f;
 
     private float targetX;
     private float targetY;
@@ -41,9 +42,9 @@ public class GameBlock extends GameBlockTemplate{
         Random randNum = new Random();
         value = (randNum.nextInt(2)+1)*2;
         mTextView.setText(String.valueOf(value));
-        mTextView.setTextSize(16);
-        mTextView.setX(coordX+TEXT_OFFSET);
-        mTextView.setY(coordY+TEXT_OFFSET);
+        mTextView.setTextSize(38);
+        mTextView.setX(coordX+xTEXT_OFFSET);
+        mTextView.setY(coordY+yTEXT_OFFSET);
         layout.addView(mTextView);
         mTextView.bringToFront();
     }
@@ -91,18 +92,18 @@ public class GameBlock extends GameBlockTemplate{
             if(getX()<targetX){
                 if(targetX<getX()+velocity){
                     setX(targetX);
-                    mTextView.setX(targetX+TEXT_OFFSET);
+                    mTextView.setX(targetX+xTEXT_OFFSET);
                 }else{
                     setX(getX()+velocity);
-                    mTextView.setX(getX()+velocity+TEXT_OFFSET);
+                    mTextView.setX(getX()+velocity+xTEXT_OFFSET);
                 }
             }else{  //Moving to the left
                 if(targetX>getX()-velocity){
                     setX(targetX);
-                    mTextView.setX(targetX+TEXT_OFFSET);
+                    mTextView.setX(targetX+xTEXT_OFFSET);
                 }else{
                     setX(getX()-velocity);
-                    mTextView.setX(getX()-velocity+TEXT_OFFSET);
+                    mTextView.setX(getX()-velocity+xTEXT_OFFSET);
                 }
             }
         }else if(getY()!=targetY){
@@ -110,18 +111,18 @@ public class GameBlock extends GameBlockTemplate{
             if(getY()<targetY){
                 if(targetY<getY()+velocity){
                     setY(targetY);
-                    mTextView.setY(targetY+TEXT_OFFSET);
+                    mTextView.setY(targetY+yTEXT_OFFSET);
                 }else{
                     setY(getY()+velocity);
-                    mTextView.setY(targetY+velocity+TEXT_OFFSET);
+                    mTextView.setY(targetY+velocity+yTEXT_OFFSET);
                 }
             }else {  //Moving to the top
                 if(targetY>getY()-velocity){
                     setY(targetY);
-                    mTextView.setY(targetY+TEXT_OFFSET);
+                    mTextView.setY(targetY+yTEXT_OFFSET);
                 }else{
                     setY(getY()-velocity);
-                    mTextView.setY(targetY-velocity+TEXT_OFFSET);
+                    mTextView.setY(targetY-velocity+yTEXT_OFFSET);
                 }
             }
         }else{  //Blocks stopped
